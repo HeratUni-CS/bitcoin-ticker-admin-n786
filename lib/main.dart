bcd44158dec66e395e9cd5fc16d17597bdd9b206
@amir320 <admin-n786>
2023-06-14 04:39:27 -0700

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -6,10 +6,12 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
-      theme: ThemeData.dark().copyWith(
-          appBarTheme: const AppBarTheme(backgroundColor: Colors.lightBlue),
-          primaryColor: Colors.lightBlue,
+  theme: ThemeData.dark().copyWith(
+
+          appBarTheme: const AppBarTheme(backgroundColor: Colors.white),
+          primaryColor: Colors.white,
           scaffoldBackgroundColor: Colors.white),
       home: PriceScreen(),
     );
